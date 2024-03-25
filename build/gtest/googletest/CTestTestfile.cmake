3e1287a6bb066083135636fb61df059781bbc1e4
Jiaxin Qiu <1986106619@qq.com>
2024-03-25 16:19:06 +0800

    delete useless files.

--- build/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/jiaxin/simulation_vedio_code/PE-Planner-final/build/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
